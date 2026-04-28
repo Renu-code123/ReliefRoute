77e4f8cf5cdc4d316ecc080daf77501e784fb7a4
Muskan Yadav <muskanyadav.maverick@gmail.com>
2026-04-28 16:04:38 +0530

    Comment out React import in Profile.tsx

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useStore } from '../store/useStore';
 import { User, Shield, Mail, LogOut, Activity, MapPin, Warehouse, RefreshCw } from 'lucide-react';
